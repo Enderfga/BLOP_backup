125a49ad8d1c79aee33b9f7a72bb09c268fc88c6
Enderfga <qq2639135175@gmail.com>
2022-04-19 20:35:08 +0800

    Site updated: 2022-04-19 20:35:07

---
+++ js/daovoice.js
@@ -0,0 +1,22 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>!(function() {
+  var appId = 'g8qn5ztp4obt0dx6ibaw2uscbcfui99mmezhirde';
+
+  (function (i, s, o, g, r, a, m) {  
+    i['DaoVoiceObject'] = r;
+    i[r] = i[r] ||
+      function () {
+        (i[r].q = i[r].q || []).push(arguments);
+      };
+    i[r].l = 1 * new Date();
+    a = s.createElement(o);
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    a.charset = 'utf-8';
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', ('https:' === document.location.protocol ? 'https:' : 'http:') + '//widget.daovoice.io/widget/' + appId + '.js', 'daovoice');
+  daovoice('init', {
+    app_id: appId,
+  });
+  daovoice('update');
+})();
