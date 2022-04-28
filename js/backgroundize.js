749eb865ac6dcc803c7c7f91e8dd22be516c7970
Enderfga <qq2639135175@gmail.com>
2022-04-29 00:34:08 +0800

    Site updated: 2022-04-29 00:34:07

--- js/backgroundize.js
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>const bannerContainer = $("#banner");
-const viewBg = $("#web_bg");
-const bannerMask = $("#banner .mask");
-const bg = $(bannerContainer).css("background-image");
-$(viewBg).css("background-image", bg);
-$(bannerContainer).css("background-image", "url()");
-const color = $(bannerMask).css("background-color");
-$(bannerMask).css("background-color", `rgba(0,0,0,0)`);
-$(viewBg).css("background-color", color);
